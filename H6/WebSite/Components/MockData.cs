05879f5b5c5ef1b9abbd5fb7d6c1ac3185c45bf0
NBraschP <nbp97@outlook.dk>
2024-05-15 11:37:56 +0200

    Question Overview and create

---
+++ H6/WebSite/Components/MockData.cs
@@ -0,0 +1,66 @@
+﻿using global::Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebSite.Components {
+        public class MockData {
+            private static MockData _instance;
+            public List<Question> _questions;
+
+            public MockData() {
+                // Initialize mock data
+                _questions = new List<Question> {
+                new Question {
+                    Id = 1,
+                    PossibleQuestion = "What is your favorite color?",
+                    Options = new List<Option>
+                    {
+                        new Option { Id = 1, PossibleOption = "Red" },
+                        new Option { Id = 2, PossibleOption = "Blue" },
+                        new Option { Id = 3, PossibleOption = "Green" }
+                    }
+                },
+                new Question {
+                    Id = 2,
+                    PossibleQuestion = "How old are you?",
+                    Options = null // No predefined options for this question
+                }
+            };
+            }
+
+            public static MockData Instance {
+                get {
+                    if (_instance == null) {
+                        _instance = new MockData();
+                    }
+                    return _instance;
+                }
+            }
+
+            public List<Question> GetQuestions() {
+                return _questions;
+            }
+
+            public void AddQuestion(Question question) {
+                // Assign a unique Id to the new question
+                question.Id = _questions.Any() ? _questions.Max(q => q.Id) + 1 : 1;
+                _questions.Add(question);
+            }
+
+            public void EditQuestion(Question editedQuestion) {
+                var existingQuestion = _questions.FirstOrDefault(q => q.Id == editedQuestion.Id);
+                if (existingQuestion != null) {
+                    existingQuestion.PossibleQuestion = editedQuestion.PossibleQuestion;
+                    existingQuestion.Options = editedQuestion.Options;
+                }
+            }
+
+            public void DeleteQuestion(int questionId) {
+                var questionToRemove = _questions.FirstOrDefault(q => q.Id == questionId);
+                if (questionToRemove != null) {
+                    _questions.Remove(questionToRemove);
+                }
+            }
+        }
+}
--- H6/WebSite/Program.cs
+++ H6/WebSite/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using WebSite.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<MockData>();
 
 var app = builder.Build();
 
