b5b331cad5ca3e8d30c1e9aae33316beb2e94a87
nick8263 <68842717+nick8263@users.noreply.github.com>
2024-05-30 09:33:41 +0200

    Update Answer.cs

--- H6/Models/Answer.cs
+++ H6/Models/Answer.cs
@@ -1,10 +1,14 @@
-﻿namespace Models
+﻿using System.Text.Json.Serialization;
+
+namespace Models
 {
     public class Answer
     {
         public int Id { get; set; }
         public Question Question { get; set; }
-        public string? FreeTextAnswer { get; set; }        
+        public string? FreeTextAnswer { get; set; }
+
+        [JsonIgnore]
         public List<AnswerGroup>? AnswerGroups { get; set; }
         public string? SelectedAnswer { get; set; }
     }
